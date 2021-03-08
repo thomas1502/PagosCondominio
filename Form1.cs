d3382123e4af9de9f72197fb52d0251db4f8db8c
Thomas BC <t.lambert.bc@gmail.com>
2021-03-08 11:07:29 -0600

    Almacenar y mostrar el listado de los datos.

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PagosCondominio
@@ -15,20 +10,86 @@ namespace PagosCondominio
         // Instanciar listas
         List<Propietario> propietarios = new List<Propietario>();
         List<Propiedad> propiedades = new List<Propiedad>();
-
+        List<Datos> datos = new List<Datos>();
         // Funciones propias
+        private void GuardarDatos()
+        {
+            FileStream stream = new FileStream("Datos.txt", FileMode.OpenOrCreate, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            // Variable "var" es capaz de almacenar cualquier tipo de dato
+            foreach (var p in datos)
+            {
+                writer.WriteLine(p.Nombre);
+                writer.WriteLine(p.Apellido);
+                writer.WriteLine(p.NumCasa);
+                writer.WriteLine(p.Cuota);
+            }
+
+            writer.Close();
+        }
         private void LeerPropietarios()
         {
+            FileStream stream = new FileStream("Propietarios.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
 
+            while (reader.Peek() > -1)
+            {
+                Propietario propietarioTemp = new Propietario();
+                propietarioTemp.Dpi = reader.ReadLine();
+                propietarioTemp.Nombre = reader.ReadLine();
+                propietarioTemp.Apellido = reader.ReadLine();
+
+                propietarios.Add(propietarioTemp);
+            }
+            reader.Close();
         }
         private void LeerPropiedades()
         {
+            FileStream stream = new FileStream("Propiedades.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
 
+            while (reader.Peek() > -1)
+            {
+                Propiedad propiedadTemp = new Propiedad();
+                propiedadTemp.NumCasa = Convert.ToInt32(reader.ReadLine());
+                propiedadTemp.Dpi = reader.ReadLine();                
+                propiedadTemp.Cuota = float.Parse(reader.ReadLine());
+
+                propiedades.Add(propiedadTemp);
+            }
+            reader.Close();
         }
         //
+        private void Mostrar()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = datos;
+            dataGridView1.Refresh();
+        }
         private void General()
         {
+            for(int x = 0; x < propietarios.Count;x++)
+            {
+                for(int y = 0; y < propiedades.Count;y++)
+                {
+                    if(propietarios[x].Dpi.Contains(propiedades[y].Dpi))
+                    {
+                        // Si se cumple la condición es porque la propiedad le pertenece
+                        // a la persona.
+                        Datos datosTemp = new Datos();
 
+                        datosTemp.Nombre = propietarios[x].Nombre;
+                        datosTemp.Apellido = propietarios[x].Apellido;
+                        datosTemp.NumCasa = propiedades[y].NumCasa;
+                        datosTemp.Cuota = propiedades[y].Cuota;
+
+                        datos.Add(datosTemp);
+                        GuardarDatos(); // Almacenar en el archivo de texto
+                    }
+                }
+            }
         }
         public Form1()
         {
@@ -46,6 +107,12 @@ namespace PagosCondominio
             if(cmbFiltro.SelectedIndex == 0)
             {
                 General();
+                Mostrar();
+            }
+            else if (cmbFiltro.SelectedIndex == 1)
+            {
+
+                Mostrar();
             }
         }
     }
